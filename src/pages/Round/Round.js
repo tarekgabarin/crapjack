3657e6abb1db77b08353c760425baf492b5533d2
tarekgabarin <tarekgabarin1992@gmail.com>
2019-08-03 14:32:46 -0400

    added reveal functionality

--- src/pages/Round/Round.js
+++ src/pages/Round/Round.js
@@ -73,6 +73,141 @@ function Round() {
 
         }, []);
 
+        function reveal(event) {
+
+            event.preventDefault();
+
+            const calculateTotalValueOfCards = (arrayOfCards, isSoftHand) => {
+
+                /// @params arrayOfCards - the array of cards belonging to either the House or Player
+                /// @params isSoftHand - A boolean value that determines whether ACE is calculated as a 1 or 11
+
+                let totalValue = 0;
+
+                for (let i = 0; i < arrayOfCards.length; i++){
+
+                    const valueOfCard = arrayOfCards[i].value;
+
+                    if (valueOfCard === 'ACE'){
+
+                        if (isSoftHand){
+                            totalValue += state.cardValuesMaps[valueOfCard][0]
+                        } else {
+                            totalValue += state.cardValuesMaps[valueOfCard][1]
+                        }
+                    } else {
+
+                        totalValue += state.cardValuesMaps[valueOfCard][0]
+
+                    }
+
+                }
+
+                return totalValue
+
+            };
+
+            const totalPlayersCardsSoftHand = calculateTotalValueOfCards(state.playerCards, true);
+            const totalPlayersCardsHardHand = calculateTotalValueOfCards(state.playerCards, false);
+            const totalHouseCardsSoftHand = calculateTotalValueOfCards(state.houseCards, true);
+            const totalHouseCardsHardHand = calculateTotalValueOfCards(state.houseCards, true);
+
+            const hasHouseGoneBust = (totalHouseCardsSoftHand > 21) && (totalHouseCardsHardHand > 21);
+            const hasPlayerGoneBust = (totalPlayersCardsSoftHand > 21) && (totalPlayersCardsHardHand > 21);
+
+            //As per the rules of BlackJack, if both the Player and House go bust, the player loses
+            if (hasHouseGoneBust && hasPlayerGoneBust){
+
+                setState({
+                    ...state,
+                    playerWon: false,
+                    gameInSession: false
+                });
+
+                /// If only Player goes bust
+            } else if (!hasHouseGoneBust && hasPlayerGoneBust) {
+
+                setState({
+                    ...state,
+                    playerWon: false,
+                    gameInSession: false
+                });
+
+                //If only House goes bust
+            } else if (hasHouseGoneBust && !hasPlayerGoneBust){
+
+                setState({
+                    ...state,
+                    playerWon: true,
+                    gameInSession: false
+                });
+
+                /// If neither the Player or House goes bust
+            }  else if (!hasPlayerGoneBust && !hasHouseGoneBust) {
+
+
+                /// Determine whether to use the softhand or hardhand for each player when deciding the winner
+                const playersOptimalValueOfCards = (totalPlayersCardsSoftHand > totalPlayersCardsHardHand) ? totalPlayersCardsSoftHand : totalPlayersCardsHardHand;
+                const housesOptimalValueOfCards = (totalHouseCardsSoftHand > totalHouseCardsHardHand) ? totalHouseCardsSoftHand : totalHouseCardsHardHand;
+
+                //In the small chance that both the player and house have the exact value of 21
+                if (playersOptimalValueOfCards === housesOptimalValueOfCards) {
+
+                    /// If its a tie then the player wins.
+                    setState({
+                        ...state,
+                        playerWon: true,
+                        gameInSession: false
+                    });
+
+                } else {
+
+                    const scoreInfo = [
+                        {
+                            player: 'House',
+                            totalCardsValue: housesOptimalValueOfCards
+                        },
+                        {
+                            player: 'Player',
+                            totalCardsValue: playersOptimalValueOfCards
+                        }
+                    ];
+
+                    /// Calculate whose total value is closer to 21, and thereby wins
+                    const winnerOfRound = scoreInfo.reduce(function (prev, curr) {
+                        return (Math.abs(curr.totalCardsValue - 21) < Math.abs(prev.totalCardsValue - 21) ? curr.player : prev.player);
+                    });
+
+                    /// If House is closer to 21, player loses
+                    if (winnerOfRound === 'House'){
+
+                        setState({
+                            ...state,
+                            playerWon: false,
+                            gameInSession: false
+                        });
+
+                        /// If Player is closer to 21 than he wins
+                    } else if (winnerOfRound === "Player") {
+
+                        setState({
+                            ...state,
+                            playerWon: true,
+                            gameInSession: false
+                        });
+                        
+                    }
+
+
+                }
+
+
+
+            }
+
+        }
+
+
         return (
             <section className={'mh7-ns mt1-ns mt4'}>
 
